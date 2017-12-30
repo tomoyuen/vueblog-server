975c5f63257303bf4fb188c4d142711d5db688f0
tomoyuen <wisyue@yahoo.com>
2017-12-30 10:58:03 +0800

    change config

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -27,7 +27,7 @@ module.exports = {
       user : 'root',
       host : '103.79.78.141',
       ref  : 'origin/master',
-      repo : 'https://github.com/tomoyuen/vueblog-server.git',
+      repo : 'git@github.com:wmui/vueblog.git',
       path : '~/www/vueblog-server',
       'post-deploy' : 'npm install && pm2 reload ecosystem.config.js --env production'
     },
@@ -35,7 +35,7 @@ module.exports = {
       user: 'root',
       host : '103.79.78.141',
       ref  : 'origin/master',
-      repo : 'https://github.com/tomoyuen/vueblog-server.git',
+      repo : 'git@github.com:wmui/vueblog.git',
       path : '~/www/vueblog-server',
       'post-deploy': 'npm install && pm2 reload ecosystem.config.js --env dev',
       env: {
